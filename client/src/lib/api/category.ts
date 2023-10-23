9a593052981b349236f1d660ec2c482bc0a694e7
harsh adhikari <harshadhikari@harshs-MacBook-Air.local>
2023-10-23 14:46:36 +0530

    add category working

---
+++ client/src/lib/api/category.ts
@@ -0,0 +1,56 @@
+import { axiosDelete, createUrl, get, patch, post } from "./api-client";
+
+export const addCategory = async (name: string) => {
+  try {
+    const response = await post(createUrl("/api/categories/"), { name });
+    return response.status === 201 ? true : false;
+  } catch (error) {
+    console.log({ error });
+  }
+};
+
+export const getAllCategories = async () => {
+  try {
+    const response = await get(createUrl("/api/categories/"));
+    if (response.data) {
+      return response.data;
+    }
+    console.log({ response });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getCategory = async (id: string) => {
+  try {
+    const response = await get(createUrl(`/api/categories/${id}`));
+    if (response.data) {
+      return response.data;
+    }
+  } catch (error) {
+    console.log({ error });
+    return error;
+  }
+};
+
+export const editCategory = async (id: string, category: string) => {
+  try {
+    const response = await patch(createUrl(`/api/categories/${id}`), {
+      name: category,
+    });
+    return response;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+};
+
+export const deleteCategory = async (id: string) => {
+  try {
+    const response = await axiosDelete(createUrl(`/api/categories/${id}`));
+    return response;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+};
